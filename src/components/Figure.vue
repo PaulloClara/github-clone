95e5f3aa3753bfe4ee119e51336b07c5d284cc3c
paulloclara <paulloclara@gmail.com>
2020-08-05 19:12:49 -0300

    Add Figure component

---
+++ src/components/Figure.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <figure class="c-figure">
+    <img :src="require(`@/assets/${$props.pSrc}`)" :alt="$attrs.alt" />
+  </figure>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Figure extends Vue {
+  @Prop({ type: String, required: true }) readonly pSrc: string;
+}
+</script>
+
+<style lang="css" scoped>
+.c-figure {
+  width: 24px;
+  height: 24px;
+}
+
+.c-figure > img {
+  width: inherit;
+  height: inherit;
+}
+</style>
