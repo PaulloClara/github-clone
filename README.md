319ea361458a0564455565245dfc055de0d6049a
paulloclara <paulloclara@gmail.com>
2020-08-05 15:59:05 -0300

    Remove unnecessary code

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# github-clone
+# GitHub Clone
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+> Recriando interface do GitHub com VueJS | UI Clone
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template lang="html">
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-
     <router-view />
   </div>
 </template>
@@ -18,23 +13,11 @@ export default class App extends Vue {}
 
 <style lang="css" scoped>
 #app {
+  color: #2c3e50;
+  text-align: center;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
