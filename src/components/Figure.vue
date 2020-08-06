f2463912d075717ec0a11cd45f4fe32dc965e4ca
paulloclara <paulloclara@gmail.com>
2020-08-06 11:04:37 -0300

    Add SVG support

--- src/components/Figure.vue
+++ src/components/Figure.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <figure class="c-figure">
-    <img :src="require(`@/assets/${$props.pSrc}`)" :alt="$attrs.alt" />
+    <svg v-if="isSvg" @load="injectSvg($event)"></svg>
+    <img v-else :src="path" :alt="$attrs.alt" />
   </figure>
 </template>
 
@@ -10,6 +11,35 @@ import { Prop, Component, Vue } from "vue-property-decorator";
 @Component
 export default class Figure extends Vue {
   @Prop({ type: String, required: true }) readonly pSrc: string;
+
+  get path() {
+    return this.isExternal ? this.pSrc : require(`@/assets/${this.pSrc}`);
+  }
+
+  get parser() {
+    return new DOMParser();
+  }
+
+  get isSvg() {
+    return this.pSrc.includes(".svg");
+  }
+
+  get isExternal() {
+    return this.pSrc.includes("http");
+  }
+
+  public async injectSvg({ target: el }) {
+    const response = await fetch(this.path);
+    const svgText = await response.text();
+    const svgDoc = this.parser.parseFromString(svgText, "image/svg+xml");
+    const svgEl = svgDoc.documentElement;
+
+    Object.keys(el.dataset).forEach(data => {
+      svgEl.dataset[data] = "";
+    });
+
+    this.$el.replaceChild(svgEl, el);
+  }
 }
 </script>
 
@@ -19,7 +49,8 @@ export default class Figure extends Vue {
   height: 24px;
 }
 
-.c-figure > img {
+.c-figure > img,
+.c-figure > svg {
   width: inherit;
   height: inherit;
 }
